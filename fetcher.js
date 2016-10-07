764e6958e3f49c454e022146c0a47da97eab75cb
Kyle Thomas <wetlife@gmail.com>
2016-10-06 17:27:00 -0700

    node-ify fetcher.js

--- fetcher.js
+++ fetcher.js
@@ -4,62 +4,35 @@ var resourceDetailURL = "https://ofmpub.epa.gov/readwebservices/v1/ResourceDetai
 var script = document.createElement('script');
 script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js';
 script.type = 'text/javascript';
-document.getElementsByTagName('head')[0].appendChild(script);
+//comment line below for use with node; investigate automation of a solution for thits
+//document.getElementsByTagName('head')[0].appendChild(script);
 
-simpleSearchClk3 = function () {
-  var name_query = " ";
-  var decision_query = "";
+searcher = function () {
   $.ajax({
     type: 'GET',
     url: resourceSearchURL,
-    data: { Name: name_query, DecisionSector:decision_query, Format: 'JSON' },
+    data: { Acronym: acronym_query, DecisionSector:decision_query, Format: 'JSON' },
     dataType: 'json',
     success: function (data) {
-      //console.log('set (auto-globally by not declaring w/ `var`): `allenRecords = []`');
-      allenRecords = [];
-      allenCheckEmail = [];
-      allenCheckTelephone = [];
+      console.log(data);
       for (i = 0; i < data.length; i++) {
-        //console.log('Searching for details of ',data[i],' with simpleSearchClk4(data[i].ResourceId);');
-        simpleSearchClk4(data[i].ResourceId);
+        snooper(data[i].ResourceId);
       }
     },
   });
 };
 
-simpleSearchClk4 = function (resourceID) {
+snooper = function (resourceID) {
   $.ajax({
     type: 'GET',
     url: resourceDetailURL,
     data: { ResourceId: resourceID, Format: 'JSON' },
-    //data: { Name: name_query, ShortDescriptionForReports: decision_query, Format: 'JSON' },
     dataType: 'json',
     success: function (data) {
-      //console.log('simpleSearchClk4(data[i].ResourceId): ',data.READExportDetail.InfoResourceDetail.GeneralDetail.LongTitleText,data);
-      //console.log('Scraping data.READExportDetail.InfoResourceDetail.ContactDetail.IndividualContactDetail.EmailAddressText: ',data.READExportDetail.InfoResourceDetail.ContactDetail.IndividualContactDetail.EmailAddressText);
-      if (data.READExportDetail.InfoResourceDetail.ContactDetail.IndividualContactDetail.FirstName == "Allen" && data.READExportDetail.InfoResourceDetail.ContactDetail.IndividualContactDetail.LastName == "Brookes") {
-        allenRecords.push(data);
-        //console.log('first = Allen and last = Brookes: pushed data object onto auto-global-list allenRecords');
-      }
-      if (data.READExportDetail.InfoResourceDetail.ContactDetail.IndividualContactDetail.EmailAddressText == "Brookes.Allen@epa.gov") {
-        allenCheckEmail.push(data);
-        //console.log('email = Brookes.Allen@epa.gov: pushed data object onto auto-global-list allenCheckEmail');
-      }
-      if (data.READExportDetail.InfoResourceDetail.ContactDetail.IndividualContactDetail.TelephoneNumber == "(541) 754-4480") {
-        allenCheckTelephone.push(data);
-        //console.log('phone = "(541) 754-4480": pushed data object onto auto-global-list allenCheckTelephone');
-      if (data.READExportDetail.InfoResourceDetail.ContactDetail.IndividualContactDetail.FirstName == "Allen" && data.READExportDetail.InfoResourceDetail.ContactDetail.IndividualContactDetail.LastName == "Brookes") {
-        allenCheck.push(data);
-        //console.log('first = Allen and last = Brookes: pushed data object onto auto-global-list allenRecords');
-      }
-      if (data.READExportDetail.InfoResourceDetail.ContactDetail.IndividualContactDetail.EmailAddressText == "allenbrookes@epa.gov") {
-        allenCheckEmail.push(data);
-        console.log('email = Brookes.Allen@epa.gov: pushed data object onto auto-global-list allenCheckEmail');
-      }
-      if (data.READExportDetail.InfoResourceDetail.ContactDetail.IndividualContactDetail.EmailAddressText == "allenbrookes@epa.gov") {
-        allenCheckTelephone.push(data);
-        console.log('phone = "(541) 754-4480": pushed data object onto auto-global-list allenCheckTelephone');
-      }
+      console.log(data);
     }
   });
 };
+
+console.log("Now running searcher() .");
+searcher();
