e99618bbab7616623cee01ad4a67ed3ea344b212
Kyle Thomas <wetlife@gmail.com>
2017-10-05 17:24:47 -0700

    check link for all ids

--- dev/devtool-spec.py
+++ dev/devtool-spec.py
@@ -8,26 +8,31 @@
 # green: simplest apparent success
 # refactor to beautify transparently
 from devtool import Devtool
+from pprint import pprint
 
 ############################################################
 # Get all Methods Straight Following PEP8. #################
 ############################################################
-# let the computer check whether all tests are passing
-def check_coverage(tests):
+def check_spec(tests):
+    '''Run every test.'''
     print('checking coverage')
     for test in tests.keys():
         if ((tests[test] == not_implemented)#"==" not "is"
         or (tests[test] is False)): #'is' works here
             print('Test failed:', test)
+            print(tests[test])
         elif verbose is True:
-            print(test, ':', tests[test])
+            print(test, ':')
+            pprint(tests[test])
 
 ############################################################
 # Define and Parameterize. #################################
 ############################################################
 verbose = True
 not_implemented = 'not implemented'
-# implement each key of tests as a function of Devtool
+url = 'http://epa.gov'
+READ_id = '12522'# READ-id of GEMIS
+# implement each key of tests as a method of Devtool
 # each should be testable in isolation
 tests = {'READ_id': not_implemented,
          'acronym': not_implemented,
@@ -36,38 +41,40 @@ tests = {'READ_id': not_implemented,
          'get_details': not_implemented,
          'link_check': not_implemented,
          'get_ids': not_implemented,
-         'validate_READ_id': not_implemented}
-url = 'http://epa.gov'
-READ_id = '12522'# READ-id of GEMIS
+         'validate_READ_id': not_implemented,
+         'check_link_for_all_ids': not_implemented,}
 
 ############################################################
-# Make the Tests that are the Spec. ########################
+# Tests are the Spec. ######################################
 ############################################################
+# return ids in statically specified decision-sectors
+tests['get_ids'] = Devtool().get_ids()
 # get details of a specified tool
 tests['get_details'] = str(Devtool().get_details(READ_id))[:50]+'...TRUNCATED...'
 # return the specified tool's READ-id 
 tests['READ_id'] = Devtool().READ_id(Devtool().get_details(READ_id))
+# validate a READ-id
+tests['validate_READ_id'] = Devtool().validate_READ_id(READ_id)
 # return the specified tool's acronym
 tests['acronym'] = Devtool().acronym(Devtool().get_details(READ_id))
-# http response's status-code
+# http response's status-code for a predefined URL
 tests['status'] = Devtool().status(url)
 # return a url-field and its value
 tests['url'] = Devtool().url(Devtool().get_details(READ_id))
 # return (read-id, acronym, url, url_status, field of url)
 tests['link_check'] = Devtool().link_check(Devtool().get_details(READ_id))
-# return ids in a specified decision-sector
-tests['get_ids'] = Devtool().get_ids()
-# validate a READ-id
-tests['validate_READ_id'] = Devtool().validate_READ_id(READ_id)
-#TODO note URL of status in a comment
-#TODO run link_check() for all ids
+# run link_check() for all ids
+tests['check_link_for_all_ids'] = Devtool().check_link_for_all_ids()
+
 #TODO send link_check() to CSV
 #TODO record final URL with status()
 #TODO compare final URL to the original
 #TODO textually describe URL-status
+#DEV TODO integrate tests into Devtool's class
+#DEV TODO determine whether to pass details or the READ-id as arg
 
 ############################################################
 # Now, with all those definitions out of the way, on to ####
 # the procedural play. #####################################
 ############################################################
-check_coverage(tests)
+spec = check_spec(tests)
--- dev/devtool.py
+++ dev/devtool.py
@@ -1,5 +1,24 @@
 class Devtool:
+
+    def probe_path(self, object, path):
+        '''Return element in given object at given path.
+
+        Attempt to access data at path in object. Give prescibed data on error.
+        '''
+        probe = object
+        for key in path:
+            if 'xsi:nil' in probe.keys():
+                return str(probe)
+            if key in probe.keys():
+                probe = probe[key]
+            else:
+                print('Report: probe_path(object, path) got to its else condition.')
+                return 'probe_path(): key ' + key + 'is not in object' + object + ' with key-set ' + probe.keys()
+        return str(probe)
+
     def get_ids(self):
+        '''Query READ's Web-Services for id of every tool in SHC's inventory.
+        '''
         from requests import get
         url_advanced_search = 'https://ofmpub.epa.gov/readwebservices/v1/ResourceAdvancedSearch'
         decision_sectors = [ 'Building+Infrastructure',
@@ -14,25 +33,80 @@ class Devtool:
             for result in results:
                 ids += [result['ResourceId']]
         return ids
+
     def get_details(self, READ_id):
+        '''Get details from READ's Web-Services with READ-id.
+        '''
         from requests import get
         details_url = 'https://ofmpub.epa.gov/readwebservices/v1/ResourceDetail?ResourceId='
         return get(details_url + str(READ_id)).json()
+
     def READ_id(self, details):
-        return details['READExportDetail']['InfoResourceDetail']['READResourceIdentifier']
+        '''Return READ-id given json-encoded details from READ's Web-Services.
+        '''
+        if type(details) != type(dict()):
+            from json import loads
+            details = loads(details)
+        READ_id = self.probe_path(details,
+                ['READExportDetail','InfoResourceDetail','READResourceIdentifier'])
+        return READ_id
+
     def acronym(self, details):
-        return details['READExportDetail']['InfoResourceDetail']['GeneralDetail']['Acronym']
+        '''Return acronym given json-encoded details from READ's Web-Services.
+        '''
+        return self.probe_path(details,
+                ['READExportDetail',
+                'InfoResourceDetail',
+                'GeneralDetail',
+                'Acronym'])
+
     def status(self, url):
+        '''Return HTTP-status-code given a URL.
+        '''
         from requests import get
-        return get(url).status_code
+        import json
+        if 'xsi:nil' in str(url):
+            return str(url)
+        try:
+            status = get(url).status_code
+        except Exception as e:
+            status = 'Devool().status(\'' + url + '\') caught an error when getting an HTTP-status-code.'
+        return status
+
     def url(self, details):
-        path_and_data = (['READExportDetail', 'InfoResourceDetail', 'AccessDetail', 'InternetDetail', 'URLText'],
-                details['READExportDetail']['InfoResourceDetail']['AccessDetail']['InternetDetail']['URLText'])
-        return path_and_data
+        '''Return url given json-encoded details from READ's Web-Services.
+        '''
+        path = ['READExportDetail',
+                'InfoResourceDetail',
+                'AccessDetail',
+                'InternetDetail',
+                'URLText']
+        return self.probe_path(details, path)
+
     def link_check(self, details):
-        url_info = self.url(details)
-        return self.READ_id(details), self.acronym(details), self.url(details)[1], self.status(self.url(details)[1]), self.url(details)[0],
+        '''Return details of link for given json-encoded details from READ's Web-Services.
+        '''
+        info = {'READ_id': self.READ_id(details),
+                'acronym': self.acronym(details),
+                'url': self.url(details),
+                'status': self.status(self.url(details)),
+                'path': ['READExportDetail', 'InfoResourceDetail', 'AccessDetail', 'InternetDetail', 'URLText'],
+               }
+        return info
+
     def validate_READ_id(self, READ_id):
+        '''Return details of link for given json-encoded details from READ's Web-Services.
+        '''
         from requests import get
         details_url = 'https://ofmpub.epa.gov/readwebservices/v1/ResourceDetail?ResourceId='
         return get(details_url + str(READ_id)).json()['READExportDetail']['InfoResourceDetail']['READResourceIdentifier']
+
+    def check_link_for_all_ids(self):
+        '''Return HTTP-status-code for url of each id in inventory.
+        '''
+        ids = self.get_ids()
+        checked_links = {}
+        for id in ids:
+            id = str(id)
+            checked_links[id] = self.link_check(self.get_details(id))
+        return checked_links
